a4534f0db20d94547fe6719b948bc94fcbf8313e
Lucas van Dam <vandam.lucas@gmail.com>
2024-10-16 18:00:15 +0200

    assimp now builds C++20

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 project "assimp"
   kind "StaticLib"
   language "C++"
-  cppdialect "C++17"
+  cppdialect "C++20"
   staticruntime "on"
 
   targetdir ("Binaries/" ..OutputDir.. "/%{prj.name}")
@@ -141,6 +141,7 @@ project "assimp"
       "contrib/rapidjson/include",
   }
 
+  filter {"files:**.cpp", "files:**.h"}
 
    filter "system:windows"
       systemversion "latest"
